4748bd4f36dc7cb588bb2915128672ee7dd5c61d
WANDERSON DESKTOP <wanmadeira@hotmail.com>
2021-03-02 23:22:37 -0300

    Ultima versão funcionando com Icones

--- UCadPrincipal.pas
+++ UCadPrincipal.pas
@@ -5,7 +5,8 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, UCadAluno2, UCadEscolaridade,
-  Vcl.Buttons, Vcl.ExtCtrls, Vcl.Menus, UCadAluno, Vcl.Imaging.jpeg;
+  Vcl.Buttons, Vcl.ExtCtrls, Vcl.Menus, UCadAluno, Vcl.Imaging.jpeg,
+  Vcl.Imaging.pngimage;
 
 type
   TPrincipal = class(TForm)
